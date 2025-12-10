a6d04c10d6b883e78a7fa2c1d7bcdad6ce40fa52
infobtpbtp <infobtpbtp@gmail.com>
2025-12-10 01:24:53 +0100

    retour achitecture

--- SCRIPTS/detailsArticle/article_architectures.js
+++ SCRIPTS/detailsArticle/article_architectures.js
@@ -96,20 +96,20 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>`;
             }
 
-            if (s2) {
-                articleDetailsItems += `
-                <div class="section-tittle mb-30 pt-30">
-                    <h3>${s2.sousTitre} </h3>
-                </div>
-                <div class="about-img">
-                    ${data.titres.imageSecondaire3 ? `<img src="${data.titres.imageSecondaire3}" alt="Image secondaire 3">` : ''}
-                </div>
-                <div class="about-prea">
-                    <p class="about-pera1 mb-25" style="font-size: 16px; text-align: justify;">
-                    ${s2.contenuSousTitre || ''}
-                    </p>
-                </div>`;
-            }
+            // if (s2) {
+            //     articleDetailsItems += `
+            //     <div class="section-tittle mb-30 pt-30">
+            //         <h3>${s2.sousTitre} </h3>
+            //     </div>
+            //     <div class="about-img">
+            //         ${data.titres.imageSecondaire3 ? `<img src="${data.titres.imageSecondaire3}" alt="Image secondaire 3">` : ''}
+            //     </div>
+            //     <div class="about-prea">
+            //         <p class="about-pera1 mb-25" style="font-size: 16px; text-align: justify;">
+            //         ${s2.contenuSousTitre || ''}
+            //         </p>
+            //     </div>`;
+            // }
 
             articleDetailsItems += `
                 <div class="social-share pt-30">
